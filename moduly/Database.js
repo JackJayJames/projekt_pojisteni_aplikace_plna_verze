e67ce13b6c4cfc5b321da49353fa3f58cd95ad1c
JackJayJames <ciklops@seznam.cz>
2023-02-01 19:11:20 +0100

    spuštění databáze

--- moduly/Database.js
+++ moduly/Database.js
@@ -4,6 +4,11 @@ const mongoose = require('mongoose');
 
 module.exports = class Database{
     constructor(){
-        this.database = "mongodb://localhost:27017/pojistdb";
+        this.database = "mongodb://127.0.0.1:27017/pojistdb";
+    }
+    spustit(){
+        mongoose.connect(this.database, { useNewUrlParser: true })
+            .then(() => console.log(`Connected to MongoDB at ${this.database}`))
+            .catch(error => console.error('Could not connect to MongoDB...', error));
     }
 }
