ca1d876b94419e6ca8773800e811acbaa44286b4
JackJayJames <ciklops@seznam.cz>
2023-02-01 18:23:28 +0100

    základní metoda post

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('hello, it works');
+});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000...'))
