c178f42a44b4173f35d3b67fbe91bd96d8ed354f
JackJayJames <ciklops@seznam.cz>
2023-02-15 21:56:00 +0100

    rozdělán ajax fetch wrapper

--- Frontend/scripty/moduly/Ajax.js
+++ Frontend/scripty/moduly/Ajax.js
@@ -1,1 +1,24 @@
 'use strict';
+
+export class Ajax{
+    static async _request(url, type, data, originalResponse){
+        let types = ['GET', 'POST', 'DELETE'];
+        type = type.toUpperCase();
+        if(types.indexOf(type) === -1)
+            throw new Error('Invalid request type');
+        
+        let fetchOptions = {
+            methon: type,
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        };
+
+        if(type === 'GET'){
+            let requestData = Ajax._serialize(data);
+        }
+    }
+}
