1f16640ef092701a5eb4b2a6cfffaadd990d2ed7
JackJayJames <ciklops@seznam.cz>
2023-02-16 20:34:51 +0100

    vytvorena privatni metoda _getPojistence

--- Frontend/scripty/moduly/ObsluhaHlStranky.js
+++ Frontend/scripty/moduly/ObsluhaHlStranky.js
@@ -6,16 +6,17 @@ const privatni = new WeakMap();
 export class ObsluhaHlStranky{
     constructor(){
         privatni.set(this, {
-
+            _getPojistence: function(){
+                Ajax.get('http://localhost:3000/api/pojistenci', { pocet: 5 })
+                    .then((data) => {
+                        console.log(data);
+                    })
+                    .catch((err) => console.log(err)) 
+            }
         });
-        this.test();
+        this.ziskatPojistence()
     }
-    test(){
-        Ajax.get('https://localhost:3000/api/pojistenci', { pocet: 5 })
-            .then((data) => {
-                console.log("Data přijata")
-                console.log(data);
-            })
-            .catch((err) => console.log(err))
+    ziskatPojistence(){
+        privatni.get(this)._getPojistence();
     }
 }
