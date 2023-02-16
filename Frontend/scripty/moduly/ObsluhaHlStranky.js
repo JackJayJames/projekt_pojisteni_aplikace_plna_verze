e4c6d30659342e3be63ecaf41f09bcc54277b128
JackJayJames <ciklops@seznam.cz>
2023-02-16 20:24:23 +0100

    rozdělán fetch

--- Frontend/scripty/moduly/ObsluhaHlStranky.js
+++ Frontend/scripty/moduly/ObsluhaHlStranky.js
@@ -4,5 +4,18 @@ import { Ajax } from "./Ajax.js";
 const privatni = new WeakMap();
 
 export class ObsluhaHlStranky{
-    
+    constructor(){
+        privatni.set(this, {
+
+        });
+        this.test();
+    }
+    test(){
+        Ajax.get('https://localhost:3000/api/pojistenci', { pocet: 5 })
+            .then((data) => {
+                console.log("Data přijata")
+                console.log(data);
+            })
+            .catch((err) => console.log(err))
+    }
 }
