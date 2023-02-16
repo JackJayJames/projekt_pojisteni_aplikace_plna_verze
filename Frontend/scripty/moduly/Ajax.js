90b9ebea15278e69192f6789927f446705e9f2ca
JackJayJames <ciklops@seznam.cz>
2023-02-16 20:03:02 +0100

    hotova metoda _request

--- Frontend/scripty/moduly/Ajax.js
+++ Frontend/scripty/moduly/Ajax.js
@@ -19,6 +19,32 @@ export class Ajax{
 
         if(type === 'GET'){
             let requestData = Ajax._serialize(data);
+            if(requestData)
+                url = (url.indexOf("?") == -1) ? `${url}?${requestData}` : `${url}&${requestData}`;
         }
+        else
+            fetchOptions.body = Ajax._serialize(data);
+        
+        let response = await fetch(url, fetchOptions);
+
+        if(originalResponse)
+            return response;
+        if(!response.ok)
+            throw new Error(`${response.status} - ${response.statusText}`);
+        
+        let contentType = response.headers.get('content-type');
+        if(contentType){
+            if(contentType.includes('application/json'))
+                return response.json();
+            if(contentType.includes('text/') || contentType.includes('text/html') || contentType.includes('text/css'
+                || contentType.includes('text/javascript') || contentType.includes('text/markdown')))
+                return response.text();
+            if(contentType.includes('multipart/form-data'))
+                return response.formData();
+            if(contentType.includes('application/octet-stream'))
+                return response.arrayBuffer();
+        }
+
+        throw new TypeError('Content type not found. Cannot autoparse the response')
     }
 }
