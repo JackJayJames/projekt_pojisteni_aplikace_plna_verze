d5604151921bc18558be2a89b9bc395e8b5a61ef
Pavel Plíhal <ciklops@seznam.cz>
2023-02-18 09:28:16 +0100

    metoda vytvoření těla tabulky

--- Frontend/scripty/moduly/TvoricTabulky.js
+++ Frontend/scripty/moduly/TvoricTabulky.js
@@ -8,6 +8,7 @@ export class TvoricTabulky{
             _vytvoritTabulku: function(data){
                 const table = document.createElement("table");
                 table.appendChild(this._vytvoritHlavicku());
+                table.appendChild(this._vytvoritTelo(data));
 
                 this._element.appendChild(table);
             },
@@ -16,6 +17,8 @@ export class TvoricTabulky{
                 const tr = document.createElement("tr");
                 tr.appendChild(this._vytvoritBunku("th", "Jméno"));
                 tr.appendChild(this._vytvoritBunku("th", "Bydliště"));
+                tr.appendChild(this._vytvoritBunku("th", "e-mail"));
+                tr.appendChild(this._vytvoritBunku("th", "telefon"));
                 tr.appendChild(this._vytvoritBunku("th", ""));
                 thead.appendChild(tr);
                 return thead;
@@ -24,6 +27,19 @@ export class TvoricTabulky{
                 const element = document.createElement(type);
                 element.textContent = text;
                 return element;
+            },
+            _vytvoritTelo: function(data){
+                const tbody = document.createElement("tbody");
+                for(const cast of data){
+                    const tr = document.createElement("tr");
+                    tr.appendChild(this._vytvoritBunku("td", `${cast.jmeno} ${cast.prijmeni}`));
+                    tr.appendChild(this._vytvoritBunku("td", cast.mesto));
+                    tr.appendChild(this._vytvoritBunku("td", cast.mail));
+                    tr.appendChild(this._vytvoritBunku("td", cast.telefon));
+                    tbody.appendChild(tr);
+                }
+
+                return tbody;
             }
         });
     }
