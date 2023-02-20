eb9a137f1531985b97c157822ea0e88d8c1457c1
JackJayJames <ciklops@seznam.cz>
2023-02-20 12:56:37 +0100

    privatni metoda _zaplnitPojistenec

--- Frontend/scripty/moduly/PridatPojistence.js
+++ Frontend/scripty/moduly/PridatPojistence.js
@@ -25,12 +25,29 @@ export class PridatPojistence{
             _errMesto: document.querySelector('#er-Mesto'),
 
             _inputPsc: document.querySelector('#poj_psc'),
-            _errPsc: document.querySelector('#er-Psc')
+            _errPsc: document.querySelector('#er-Psc'),
+
+            _pojistenec: {},
+
+            _zaplnitPojistence: function(){
+                this._pojistenec.jmeno = this._inputJmeno.value;
+                this._pojistenec.prijmeni = this._inputPrijmeni.value;
+                this._pojistenec.email = this._inputEmail.value;
+                this._pojistenec.telefon = this._inputTelefon.value;
+                this._pojistenec.ulice = this._inputUlice.value;
+                this._pojistenec.mesto = this._inputMesto.value;
+                this._pojistenec.psc = this._inputPsc.value;
+                
+                this._zvalidovat();
+            },
+            _zvalidovat: function(){
+                console.log(this._pojistenec);
+            }
         });
     }
     spustit(){
         privatni.get(this)._ulozitTlacitko.onclick = () => {
-            console.log(this);
+            privatni.get(this)._zaplnitPojistence();
         }
     }
 }
