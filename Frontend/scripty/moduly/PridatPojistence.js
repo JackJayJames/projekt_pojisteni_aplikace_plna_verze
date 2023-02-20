7fe3dc9d7aee3981b09ac76cb7108f445393a3ff
JackJayJames <ciklops@seznam.cz>
2023-02-20 12:41:52 +0100

    přidány selectory na elementy do třídy

--- Frontend/scripty/moduly/PridatPojistence.js
+++ Frontend/scripty/moduly/PridatPojistence.js
@@ -4,7 +4,28 @@ const privatni = new WeakMap();
 export class PridatPojistence{
     constructor(){
         privatni.set(this, {
-            _ulozitTlacitko: document.querySelector("#ulozitPojistence"),
+            _ulozitTlacitko: document.querySelector('#ulozitPojistence'),
+
+            _inputJmeno: document.querySelector('#poj_jmeno'),
+            _errJmeno: document.querySelector('#er-Jmeno'),
+
+            _inputPrijmeni: document.querySelector('#poj_prijmeni'),
+            _errPrijmeni: document.querySelector('#er-Prijmeni'),
+
+            _inputEmail: document.querySelector('#poj_email'),
+            _errEmail: document.querySelector('#er-Email'),
+
+            _inputTelefon: document.querySelector('#poj_telefon'),
+            _errTelefon: document.querySelector('#er-Telefon'),
+
+            _inputUlice: document.querySelector('#poj_ulice'),
+            _errUlice: document.querySelector('#er-Ulice'),
+
+            _inputMesto: document.querySelector('#poj_mesto'),
+            _errMesto: document.querySelector('#er-Mesto'),
+
+            _inputPsc: document.querySelector('#poj_psc'),
+            _errPsc: document.querySelector('#er-Psc')
         });
     }
     spustit(){
