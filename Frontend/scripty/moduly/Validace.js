0393bd674f0e471098f3249c90a13ba3407654bf
Pavel Plíhal <ciklops@seznam.cz>
2023-02-20 18:51:33 +0100

    metody validace emailu

--- Frontend/scripty/moduly/Validace.js
+++ Frontend/scripty/moduly/Validace.js
@@ -4,24 +4,47 @@ export class Validace{
     static #_response;
 
     static zvalidovat(pojistenec){
-        this.#_response = { status: "OK" };
+        this.#_response = { status: true };
         this.#_response.jmeno = this.#_zvalidovatJmeno(pojistenec.jmeno);
         this.#_response.prijmeni = this.#_zvalidovatJmeno(pojistenec.prijmeni);
+        this.#_response.email = this.#_zvalidovatEmail(pojistenec.email);
         console.log(this.#_response);
         return this.#_response;
     }
     static #_zvalidovatJmeno(jmeno){
         if(!jmeno){
-            this.#_response.status = "notOK";
+            this.#_response.status = false;
             return "Toto pole je povinné";
         }
         if(!this.#_delka(jmeno, 2, 30)){
-            this.#_response.status = "notOK";
+            this.#_response.status = false;
             return "Délka musí být delší než 1 a kratší než 31";
         }
         if(!this.#_jednoSlovo(jmeno)){
-            this.#_response.status = "notOK";
+            this.#_response.status = false;
             return "Musí obsahovat jedno slovo";
+        }
+    }
+    static #_zvalidovatEmail(mail){
+        if(!mail){
+            this.#_response.status = false;
+            return "Toto pole je povinné";
+        }
+        if(!this.#_delka(mail, 2, 30)){
+            this.#_response.status = false;
+            return "Délka musí být delší než 1 a kratší než 31";
+        }
+        if(!this.#_jednoSlovo(mail)){
+            this.#_response.status = false;
+            return "Musí obsahovat jedno slovo";
+        }
+        if(!this.#_obsahujeZnaky(["@", "."], mail)){
+            this.#_response.status = false;
+            return "Musí obsahovat znaky @ a .";
+        }
+        if(!this.#_schemaEmail(mail)){
+            this.#_response.status = false;
+            return "Invalidní schéma E-mailu";
         }
     }
 
@@ -30,5 +53,27 @@ export class Validace{
     }
     static #_jednoSlovo(slovo){
         return slovo.trim().split(" ").length == 1;
+    }
+    static #_obsahujeZnaky(znaky, mail){
+        for(const znak of znaky){
+            if(mail.indexOf(znak) == -1)   return false;
+        }
+        return true;
+    }
+    static #_schemaEmail(mail){
+        mail = mail.trim();
+        const arr = mail.split("@").join(".").split('.');
+        if(arr.length !== 3)    return false;
+        if(arr.some((a) => a === ""))   return false;
+        if(!this.#_obasahujePismenaCisla(arr.join(""))) return false;
+        if(!this.#_obasahujePismena(arr[2])) return false;
+
+        return true;
+    }
+    static #_obasahujePismenaCisla(string){
+        return /^[A-Za-z0-9]*$/.test(string);
+    }
+    static #_obasahujePismena(string){
+        return /^[A-Za-z]*$/.test(string);
     }
 }
