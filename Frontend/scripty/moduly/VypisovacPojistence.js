0397deac31c4c9f2996ffa45d913bbc78bbe496b
Pavel Plíhal <ciklops@seznam.cz>
2023-03-06 18:03:07 +0100

    zobrazeni detailu pojistence

--- Frontend/scripty/moduly/VypisovacPojistence.js
+++ Frontend/scripty/moduly/VypisovacPojistence.js
@@ -6,7 +6,7 @@ export class VypisovacPojistence{
         privatni.set(this, {
             _jmeno: document.querySelector("#jmeno"),
             _prijmeni: document.querySelector("#prijmeni"),
-            _narozeni: document.querySelector("narozeni"),
+            _narozeni: document.querySelector("#narozeni"),
             _mail: document.querySelector("#mail"),
             _telefon: document.querySelector("#telefon"),
             _ulice: document.querySelector("#ulice"),
@@ -14,7 +14,14 @@ export class VypisovacPojistence{
             _psc: document.querySelector("#psc"),
 
             _vypsatPojistence: function(pojistenec){
-                
+                this._jmeno.textContent = pojistenec.jmeno;
+                this._prijmeni.textContent = pojistenec.prijmeni;
+                this._narozeni.textContent = new Date(pojistenec.narozeni).toLocaleDateString();
+                this._mail.textContent = pojistenec.mail;
+                this._telefon.textContent = pojistenec.telefon;
+                this._ulice.textContent = pojistenec.ulice;
+                this._mesto.textContent = pojistenec.mesto;
+                this._psc.textContent = pojistenec.psc;
             }
         });
     }
