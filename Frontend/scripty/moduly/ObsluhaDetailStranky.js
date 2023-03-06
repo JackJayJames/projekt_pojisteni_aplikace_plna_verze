5fc4453e66e59778c69b446d28b8981476d4a1ab
Pavel Plíhal <ciklops@seznam.cz>
2023-03-06 17:42:59 +0100

    export import třídy VypisovacPojistence

--- Frontend/scripty/moduly/ObsluhaDetailStranky.js
+++ Frontend/scripty/moduly/ObsluhaDetailStranky.js
@@ -1,20 +1,25 @@
 'use strict';
 
 import { Ajax } from "./Ajax.js";
+import { VypisovacPojistence } from "./VypisovacPojistence.js";
 
 const privatni = new WeakMap();
 export class ObsluhaDetailStranky{
     constructor(id){
         privatni.set(this, {
             _id: id,
+            _data: {},
 
             _ziskatPojistence: function(){
                 Ajax.get(`http://localhost:5500/api/pojistenec/${this._id}`, { pocet: 5 })
                 .then((data) => {
                     this._data = data;
-                    console.log(this._data);
+                    this._zpracovatData();
                 })
                 .catch((err) => console.log(err)) 
+            },
+            _zpracovatData: function(){
+                console.log(this._data);
             }
         });
     }
