b9e396cf23e02814d10ca172f17854f3b561db0a
Pavel Plíhal <ciklops@seznam.cz>
2023-03-06 16:53:05 +0100

    ulozeni id do privatni vlastnosti

--- Frontend/scripty/moduly/ObsluhaDetailStranky.js
+++ Frontend/scripty/moduly/ObsluhaDetailStranky.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const privatni = new WeakMap();
 export class ObsluhaDetailStranky{
     constructor(id){
-        
+        privatni.set(this, {
+            _id: id
+        });
     }
 }
