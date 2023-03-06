ab253ed000da89e44797b461b286bc7106b5f11b
Pavel Plíhal <ciklops@seznam.cz>
2023-03-06 16:49:16 +0100

    získání id ze sessionStorage

--- Frontend/scripty/detail_stranka/main.js
+++ Frontend/scripty/detail_stranka/main.js
@@ -1,3 +1,5 @@
 'use strict';
 
 import { ObsluhaDetailStranky } from "../moduly/ObsluhaDetailStranky.js";
+
+const detail = new ObsluhaDetailStranky(sessionStorage.getItem("pojistenec"));
--- Frontend/scripty/moduly/ObsluhaDetailStranky.js
+++ Frontend/scripty/moduly/ObsluhaDetailStranky.js
@@ -1,5 +1,7 @@
 'use strict';
 
 export class ObsluhaDetailStranky{
-    
+    constructor(id){
+        
+    }
 }
