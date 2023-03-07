c2dfd0e4563563d2697b3314d5db35634d54857e
JackJayJames <ciklops@seznam.cz>
2023-03-07 07:43:12 +0100

    pridan construktor do třídy Pojisteni

--- Frontend/scripty/moduly/Pojisteni.js
+++ Frontend/scripty/moduly/Pojisteni.js
@@ -1,5 +1,11 @@
 'use strict';
 
 export class Pojisteni{
-    
+    constructor(nazev, castka, predmet, platnost){
+        this.nazev = nazev;
+        this.castka = castka;
+        this.predmet = platnost;
+        this.platnost_od = new Date.now();
+        this.platnost_do = platnost;
+    }
 }
