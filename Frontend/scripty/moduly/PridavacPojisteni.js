073c6ed5a7f874c21852b7dbaa3ec14d97dd681d
JackJayJames <ciklops@seznam.cz>
2023-03-07 07:18:31 +0100

    přidány selectory do PridavacePojisteni

--- Frontend/scripty/moduly/PridavacPojisteni.js
+++ Frontend/scripty/moduly/PridavacPojisteni.js
@@ -1,5 +1,13 @@
 'use strict';
 
+const privatni = new WeakMap();
 export class PridavacPojisteni{
-
+    constructor(){
+        privatni.set(this, {
+            _nazev: document.querySelector("#nazev"),
+            _castka: document.querySelector("#castka"),
+            _predmet: document.querySelector("#predmet"),
+            _platnost: document.querySelector("#platnost")
+        });
+    }
 }
