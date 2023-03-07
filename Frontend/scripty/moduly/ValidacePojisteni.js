8c3f5aa3c95b677f1994c5ec89fc403e265e18eb
JackJayJames <ciklops@seznam.cz>
2023-03-07 08:45:21 +0100

    validace casky a datumu

--- Frontend/scripty/moduly/ValidacePojisteni.js
+++ Frontend/scripty/moduly/ValidacePojisteni.js
@@ -7,14 +7,16 @@ export class ValidacePojisteni{
         this.#_vysledek = { status: true };
 
         this.#_vysledek.nazev = this.#_validaceNazvu(pojisteni.nazev);
+        this.#_vysledek.castka = this.#_validaceCastky(pojisteni.castka);
         this.#_vysledek.predmet = this.#_validaceNazvu(pojisteni.predmet);
+        this.#_vysledek.platnost_do = this.#_validaceDatumu(pojisteni.platnost_do);
 
         return this.#_vysledek;
     }
     static #_validaceNazvu(nazev){
         if(!nazev){
             this.#_vysledek.status = false;
-            return "Toto pole je poviné";
+            return "Toto pole je povinné";
         }
         if(nazev.length < 3){
             this.#_vysledek.status = false;
@@ -29,8 +31,38 @@ export class ValidacePojisteni{
             return "Může obsahovat jenom písmena";
         }
     }
+    static #_validaceCastky(castka){
+        if(!castka){
+            this.#_vysledek.status = false;
+            return "Toto pole je povinné";
+        }
+        if(this.#_jenomCisla(castka)){
+            this.#_vysledek.status = false;
+            return "Může obsahovat jenom čísla";
+        }
+        if(castka > 0){
+            this.#_vysledek.status = false;
+            return "Číslo musí být větší než 0";
+        }
+    }
+    static #_validaceDatumu(datum){
+        if(!datum){
+            this.#_vysledek.status = false;
+            return "Toto pole je povinné";
+        }
+        if(this.#_vBudoucnu(datum)){
+            this.#_vysledek.status = false;
+            return "Datum je v minulosti";
+        }
+    }
 
     static #_jenomPismena(str){
         return !/^[A-Za-zÁ-Žá-ž]*$/.test(str);
+    }
+    static #_jenomCisla(cislo){
+        return !/^[0-9]*$/.test(cislo);
+    }
+    static #_vBudoucnu(datum){
+        return new Date(datum) < Date.now();
     }
 }
