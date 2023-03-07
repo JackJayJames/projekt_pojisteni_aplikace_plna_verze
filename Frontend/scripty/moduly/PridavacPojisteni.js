d235894939a7599395c3d637b12e4ad665210d15
JackJayJames <ciklops@seznam.cz>
2023-03-07 08:06:11 +0100

    modul ValidacePojisteni.js

--- Frontend/scripty/moduly/PridavacPojisteni.js
+++ Frontend/scripty/moduly/PridavacPojisteni.js
@@ -15,7 +15,12 @@ export class PridavacPojisteni{
             
             _vytvoritPojisteni: function(){
                 const d = new Date();
-                return this._pojisteni = new Pojisteni(this._nazev.value, this._castka.value, this._predmet.value, `${d.getFullYear()}-${d.getMonth()+1 < 10 ? '0' + (d.getMonth()+1) : d.getMonth()+1}-${d.getDate() < 10 ? '0' + d.getDate() : d.getDate()}`, this._platnost.value);
+                this._pojisteni = new Pojisteni(this._nazev.value, this._castka.value, this._predmet.value, `${d.getFullYear()}-${d.getMonth()+1 < 10 ? '0' + (d.getMonth()+1) : d.getMonth()+1}-${d.getDate() < 10 ? '0' + d.getDate() : d.getDate()}`, this._platnost.value);
+                const validace = this._zvalidovat();
+                
+            },
+            _zvalidovat: function(){
+                
             }
         });
     }
