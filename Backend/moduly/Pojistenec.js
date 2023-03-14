d19c3abbf36fc73bf5e6b5a3627296d1f88afd63
Pavel Plíhal <ciklops@seznam.cz>
2023-03-14 11:42:20 +0100

    vytvořena třída pojištěnce

---
+++ Backend/moduly/Pojistenec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+export class Pojistenec{
+    constructor(jmeno, prijmeni, narozeni, mail, telefon, ulice, mesto, psc, pojisteni){
+        this.jmeno = jmeno;
+        this.prijmeni = prijmeni;
+        this.narozeni = narozeni;
+        this.mail = mail;
+        this.telefon = telefon;
+        this.ulice = ulice;
+        this.mesto = mesto;
+        this.psc = psc;
+        this.pojisteni = pojisteni;
+    }
+}
