9016e0daf7127c0cfa793028a3ee2c71c2623983
JackJayJames <ciklops@seznam.cz>
2023-03-17 17:17:14 +0100

    smazany metody třidy ObsluhaHlStranky

--- Frontend/scripty/moduly/ObsluhaHlStranky.js
+++ Frontend/scripty/moduly/ObsluhaHlStranky.js
@@ -6,59 +6,6 @@ const privatni = new WeakMap();
 
 export class ObsluhaHlStranky{
     constructor(){
-        privatni.set(this, {
-            _tabulka: new TvoricTabulky(document.querySelector(".con-tabulka")),
-            _data: [],
-
-            _getPojistence: function(){
-                Ajax.get('http://localhost:5500/api/pojistenci', { pocet: 5 })
-                    .then((data) => {
-                        this._data = data;
-                        this._vykreslitPojistence();
-                    })
-                    .catch((err) => console.log(err)) 
-            },
-            _deletePojistence: function(id){
-                fetch(`http://localhost:5500/api/pojistenec/${id}`, {
-                    method: 'DELETE',
-                    mode: 'cors',
-                    cache: 'no-cache',
-                    credentials: 'same-origin',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                    body: "",
-                })
-                    .then(res => {
-                        this._getPojistence();
-                    })
-                    .catch(err => console.log(err));
-            },
-            _vykreslitPojistence: function(){
-                this._tabulka.vytvorit(this._data);
-                this._obsluhaDeleteTlacitek();
-                this._obsluhaDetailu();
-            },
-            _obsluhaDeleteTlacitek: function(){
-                for(const pojistenec of this._data){
-                    document.getElementById(pojistenec._id).onclick = (e) => {
-                        this._deletePojistence(e.target.id);
-                    };
-                }
-            },
-            _obsluhaDetailu: function(){
-                const pojistenci = document.querySelectorAll('.pojJmeno');
-                for(const pojistenec of pojistenci){
-                    pojistenec.onclick = (e) => {
-                        sessionStorage.setItem("pojistenec", e.target.id.replace("poj-", ""));
-                        window.location.href = "./detail.html";
-                    };
-                }
-            }
-        });
-        this.vypsatPojistence()
-    }
-    vypsatPojistence(){
-        privatni.get(this)._getPojistence();
+        
     }
 }
