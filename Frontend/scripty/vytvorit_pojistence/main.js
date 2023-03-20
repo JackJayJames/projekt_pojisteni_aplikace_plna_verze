12799034e67242905691d844cc90061cae61a063
JackJayJames <ciklops@seznam.cz>
2023-03-20 06:43:57 +0100

    vytvořen objekt instancí tříd FormIntputOutput pro konstruktor

--- Frontend/scripty/vytvorit_pojistence/main.js
+++ Frontend/scripty/vytvorit_pojistence/main.js
@@ -3,8 +3,19 @@
 import { PridatPojistence } from "../moduly/PridatPojistence.js";
 import { FormInputOutput } from "../moduly/FormInputOutput.js";
 
-const username = new FormInputOutput(document.querySelector('#poj_username'), document.querySelector('#er-Username'));
-console.log(username);
+const adresy = {
+    username: new FormInputOutput(document.querySelector('#poj_username'), document.querySelector('#er-Username')),
+    password_I: new FormInputOutput(document.querySelector('#poj_password'), document.querySelector('#er-Password')),
+    password_II: new FormInputOutput(document.querySelector('#poj_passwordII'), document.querySelector('#er-PasswordII')),
+    jmeno: new FormInputOutput(document.querySelector('#poj_jmeno'), document.querySelector('#er-Jmeno')),
+    prijmeni: new FormInputOutput(document.querySelector('#poj_prijmeni'), document.querySelector('#er-Prijmeni')),
+    email: new FormInputOutput(document.querySelector('#poj_email'), document.querySelector('#er-Email')),
+    telefon: new FormInputOutput(document.querySelector('#poj_telefon'), document.querySelector('#er-Telefon')),
+    ulice: new FormInputOutput(document.querySelector('#poj_ulice'), document.querySelector('#er-Ulice')),
+    mesto: new FormInputOutput(document.querySelector('#poj_mesto'), document.querySelector('#er-Mesto')),
+    narozeni: new FormInputOutput(document.querySelector('#poj_narozeni'), document.querySelector('#er-Narozeni')),
+    psc: new FormInputOutput(document.querySelector('#poj_psc'), document.querySelector('#er-Psc'))
+};
 
-const pridatPojistence = new PridatPojistence();
+const pridatPojistence = new PridatPojistence(adresy, document.querySelector('#ulozitPojistence'));
 pridatPojistence.spustit();
