0d0f21a3d06b804a9c8ce80a7ed6b2a3239c6fd4
JackJayJames <ciklops@seznam.cz>
2023-03-20 06:48:16 +0100

    vlastnosti udělány privatni

--- Frontend/scripty/moduly/PridatPojistence.js
+++ Frontend/scripty/moduly/PridatPojistence.js
@@ -3,11 +3,13 @@
 import { Validace } from "./Validace.js";
 
 export class PridatPojistence{
+    #adresy;
+    #ulozitTl;
     constructor(adresy, ulozitTl){
-        this.adresy = adresy;
-        this.ulozitTl = ulozitTl;
+        this.#adresy = adresy;
+        this.#ulozitTl = ulozitTl;
     }
     spustit(){
-        console.log("PridatPojistence.js spusteno");
+        console.log(this.#adresy);
     }
 }
