e8e3ebc2e9984f827abea22268e34297957791a4
JackJayJames <ciklops@seznam.cz>
2023-03-20 06:27:13 +0100

    export, inport modulu

--- Frontend/scripty/vytvorit_pojistence/main.js
+++ Frontend/scripty/vytvorit_pojistence/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import { PridatPojistence } from "../moduly/PridatPojistence.js";
+import { FormInput } from "../moduly/FormInput.js";
 
 const pridatPojistence = new PridatPojistence();
 pridatPojistence.spustit();
