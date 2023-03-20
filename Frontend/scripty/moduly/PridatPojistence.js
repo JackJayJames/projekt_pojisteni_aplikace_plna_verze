6e626aed8d3f8a3861f617250d6f10ad5d7af5ab
JackJayJames <ciklops@seznam.cz>
2023-03-20 06:45:14 +0100

    vytvořen kontruktor třídy PridatPojistence

--- Frontend/scripty/moduly/PridatPojistence.js
+++ Frontend/scripty/moduly/PridatPojistence.js
@@ -3,7 +3,10 @@
 import { Validace } from "./Validace.js";
 
 export class PridatPojistence{
-
+    constructor(adresy, ulozitTl){
+        this.adresy = adresy;
+        this.ulozitTl = ulozitTl;
+    }
     spustit(){
         console.log("PridatPojistence.js spusteno");
     }
