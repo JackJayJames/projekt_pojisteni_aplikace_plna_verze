94ae1163f4354e0542277eb5d2a030f3ebbe26ff
Pavel Plíhal <ciklops@seznam.cz>
2023-03-23 18:41:55 +0100

    přidána podmínka pro detail.html

--- Frontend/scripty/detail_stranka/main.js
+++ Frontend/scripty/detail_stranka/main.js
@@ -2,5 +2,17 @@
 
 import { ObsluhaDetailStranky } from "../moduly/ObsluhaDetailStranky.js";
 
-const detail = new ObsluhaDetailStranky(sessionStorage.getItem("pojistenec"));
-detail.spustit();
+const userData = {
+    "pojistenec_id": localStorage.getItem('pojistenec_id'),
+    "ticket_id": localStorage.getItem('ticket_id')
+};
+console.log(userData);
+
+if(userData.pojistenec_id && userData.ticket_id){
+    const detail = new ObsluhaDetailStranky(sessionStorage.getItem("pojistenec"));
+    detail.spustit();
+}
+else{
+    window.location.replace('./index.html');
+}
+
