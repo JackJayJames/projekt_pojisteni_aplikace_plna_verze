76bf523d4f9c75914c870f99f55d9b4374fe6fe2
Pavel Plíhal <ciklops@seznam.cz>
2023-03-23 19:05:58 +0100

    vytvořena třída Vlastnosti

--- Frontend/scripty/moduly/Vlastnosti.js
+++ Frontend/scripty/moduly/Vlastnosti.js
@@ -1,2 +1,10 @@
 'use strict';
 
+export class Vlastnosti{
+    #vlastnost;
+    #adresa;
+    constructor(vlastnost, adresa){
+        this.#vlastnost = vlastnost;
+        this.#odresa = adresa;
+    }
+}
