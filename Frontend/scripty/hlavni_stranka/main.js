1f8c4dfcb6a475bdf2807d81517e18569887f085
JackJayJames <ciklops@seznam.cz>
2023-03-27 23:14:54 +0200

    update

--- Frontend/scripty/hlavni_stranka/main.js
+++ Frontend/scripty/hlavni_stranka/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+import { FormInputOutput } from "../moduly/FormInputOutput.js";
 import { ObsluhaHlStranky } from "../moduly/ObsluhaHlStranky.js";
+import { Validace } from "../moduly/Validace.js";
 
-const obsluha = new ObsluhaHlStranky();
+const loginInput = {
+    username: new FormInputOutput(document.querySelector('[name="Username"]'), document.querySelector('.err-username'), Validace.zvalidovatUsername),
+    password: new FormInputOutput(document.querySelector('[name="Password"]'), document.querySelector('.err-password'), Validace.zvalidovatPassword)
+};
+
+const obsluha = new ObsluhaHlStranky(loginInput, document.querySelector(".prihlasit"));
+obsluha.spustit();
--- Frontend/scripty/moduly/ObsluhaHlStranky.js
+++ Frontend/scripty/moduly/ObsluhaHlStranky.js
@@ -1,11 +1,34 @@
 'use strict';
 
 import { Ajax } from "./Ajax.js";
-import { TvoricTabulky } from "./TvoricTabulky.js";
-const privatni = new WeakMap();
+import { PopUp } from "../moduly/PopUp.js";
 
 export class ObsluhaHlStranky{
+<<<<<<< Updated upstream
     constructor(){
         
+=======
+    #login;
+    #loginBtn;
+    #popUp;
+    constructor(login, button){
+        this.#login = login;   
+        this.#loginBtn = button;
+        this.#popUp = new PopUp(document.querySelector('.container'));
+    }
+    #validaceLogin(){
+        const validationResults = [];
+        for(const form in this.#login){
+            console.log(form);
+            validationResults.push(this.#login[form].validni);
+        }
+        console.log(validationResults);
+        return true;
+    }
+    spustit(){
+        this.#loginBtn.onclick = () => {
+            if(this.#validaceLogin())    return this.#popUp.error('Invalidni Login', 123456789);
+        };
+>>>>>>> Stashed changes
     }
 }
